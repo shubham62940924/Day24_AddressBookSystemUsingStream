c6e3368b20bc700c9bdd4e7104f8f83b627f1105
first_name <ss329222@gmail.com>
2022-03-19 15:18:18 +0530

    Added:AddressBookSystemUsingStream

---
+++ src/com/blz/stream/AddressBookMain.java
@@ -0,0 +1,27 @@
+package com.blz.stream;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+
+		InterfaceOne person1 = new Person();
+		person1.createContact();
+		person1.addContact();
+		person1.editContact();
+		person1.deleteContact();
+		person1.display();
+		person1.addToBook();
+		person1.operation();
+		person1.displayBook();
+		person1.searchPerson();
+		person1.addContactByCity();
+		person1.viewByCity();
+		person1.addContactByState();
+		person1.viewByState();
+		person1.sortAddressBook();
+		person1.sortByCity();
+		person1.sortByState();
+		person1.writeToAddressBookFile();
+		person1.readDataFromFile();
+	}
+
+}
---
+++ src/com/blz/stream/InterfaceOne.java
@@ -0,0 +1,43 @@
+package com.blz.stream;
+
+import java.util.List;
+
+public interface InterfaceOne {
+
+	void createContact();
+
+	void addContact();
+
+	void editContact();
+
+	void deleteContact();
+
+	void display();
+
+	void displayBook();
+
+	void addToBook();
+
+	void operation();
+
+	void searchPerson();
+
+	void addContactByCity();
+
+	void viewByCity();
+
+	void addContactByState();
+
+	void viewByState();
+
+	void sortAddressBook();
+
+	void sortByCity();
+
+	void sortByState();
+
+	void writeToAddressBookFile();
+
+	public List<String> readDataFromFile();
+
+}
